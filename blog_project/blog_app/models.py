a7ddadafefae1b2a0ef3cb4c41dd9012d27f6c58
SahhaShin <tksgk2598@naver.com>
2020-07-26 23:48:25 +0900

    django_blog_01

---
+++ blog_project/blog_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    subject=models.CharField(max_length=50, blank=False)
+    created_date=models.DateTimeField(null=True, blank=True)
+    memo=models.CharField(max_length=200, blank=False)
+    def __str__(self):
+        return self.subject
